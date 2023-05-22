57828ba27b5d3acc7ed59216991e0b0cc0a94045
권상구 <milennium9@startergames.com>
2023-05-23 06:32:07 +0900

    Setting asset bug fix.

--- Editor/GptApiSettingsEditor.cs
+++ Editor/GptApiSettingsEditor.cs
@@ -2,27 +2,11 @@ using System.Collections.Generic;
 using unity_gpt_api.Runtime;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace unity_gpt_api.Editor {
     public class GptApiSettingsEditor : SettingsProvider {
         public static GptApiSettings Settings { get; private set; }
-
-        [InitializeOnLoadMethod]
-        private static void Initialize() {
-            if (Settings != null) return;
-            
-            var directoryPath = System.IO.Path.GetDirectoryName(GptApiSettings.SettingsPath);
-            if (!AssetDatabase.IsValidFolder(directoryPath))
-            {
-                AssetDatabase.CreateFolder(System.IO.Path.GetDirectoryName(directoryPath), System.IO.Path.GetFileName(directoryPath));
-            }
-            Settings = AssetDatabase.LoadAssetAtPath<GptApiSettings>(GptApiSettings.SettingsPath);
-            
-            if (Settings != null) return;
-            Settings = ScriptableObject.CreateInstance<GptApiSettings>();
-            AssetDatabase.CreateAsset(Settings, GptApiSettings.SettingsPath);
-            AssetDatabase.SaveAssets();
-        }
         
         [SettingsProvider]
         public static SettingsProvider CreateProtobufBuilderSettingsProvider() {
@@ -38,6 +22,39 @@ namespace unity_gpt_api.Editor {
         }
 
 	    public GptApiSettingsEditor(string path, SettingsScope scopes = SettingsScope.Project, IEnumerable<string> keywords = null) : base(path, scopes, keywords) {
+        }
+        
+        public static void CreateFolderRecursively(string path)
+        {
+            var parts = path.Split('/');
+            var currentPath = "";
+    
+            for (var i = 0; i < parts.Length; i++)
+            {
+                currentPath += parts[i];
+                if (!AssetDatabase.IsValidFolder(currentPath))
+                {
+                    var parentPath = string.Join("/", parts, 0, i);
+                    AssetDatabase.CreateFolder(parentPath, parts[i]);
+                }
+                currentPath += "/";
+            }
+        }
+
+        public override void OnActivate(string searchContext, VisualElement rootElement) {
+            if (Settings != null && !string.IsNullOrEmpty(AssetDatabase.GetAssetPath(Settings)) ) return;
+            
+            var directoryPath = System.IO.Path.GetDirectoryName(GptApiSettings.SettingsPath);
+            if (!AssetDatabase.IsValidFolder(directoryPath))
+            {
+                CreateFolderRecursively(directoryPath);
+            }
+            Settings = AssetDatabase.LoadAssetAtPath<GptApiSettings>(GptApiSettings.SettingsPath);
+            
+            if (Settings != null) return;
+            Settings = ScriptableObject.CreateInstance<GptApiSettings>();
+            AssetDatabase.CreateAsset(Settings, GptApiSettings.SettingsPath);
+            AssetDatabase.SaveAssets();
         }
 
         public override void OnGUI(string searchContext) {
--- Runtime/GptApiSettings.cs
+++ Runtime/GptApiSettings.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 namespace unity_gpt_api.Runtime {
     public class GptApiSettings : ScriptableObject {
         public const string SettingFileName = "GptApiSettings";
-        public const string SettingsPath = "Assets/GptApi/Resources/" + SettingFileName + ".asset";
-        
+        public const string SettingsDirectory = "Assets/GptApi/Resources/";
+        public const string SettingsPath = SettingsDirectory + SettingFileName + ".asset";
+
         public string apiKey = "";
         public string organizationId = "";
     }
